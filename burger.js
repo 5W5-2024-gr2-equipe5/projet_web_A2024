c179930e839c3f561f860a44e5a38768b46b435b
EricNguyen0 <e1446191@cmaisonneuve.qc.ca>
2024-09-13 15:22:33 -0400

    menu plus vivant

--- burger.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const burger = document.getElementById('openBtn');
-  const menu = document.querySelector('.menu');
-
-  burger.addEventListener('click', function() {
-    menu.classList.toggle('active');
-    burger.classList.toggle('open');
-  });
-});
