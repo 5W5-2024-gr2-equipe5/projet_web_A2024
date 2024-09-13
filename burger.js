1457212c807fb1f5dc69740676bd02d564bfdfa9
EricNguyen0 <e1446191@cmaisonneuve.qc.ca>
2024-09-13 14:49:20 -0400

    button burger animer

--- burger.js
+++ burger.js
@@ -1,15 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const openBtn = document.getElementById("openBtn");
-  const closeBtn = document.getElementById("closeBtn");
-  const menu = document.querySelector(".menu");
+document.addEventListener('DOMContentLoaded', function() {
+  const burger = document.getElementById('openBtn');
+  const menu = document.querySelector('.menu');
 
-  openBtn.addEventListener("click", (event) => {
-    event.preventDefault();
-    menu.classList.add("active");
-  });
-
-  closeBtn.addEventListener("click", (event) => {
-    event.preventDefault();
-    menu.classList.remove("active");
+  burger.addEventListener('click', function() {
+    menu.classList.toggle('active');
+    burger.classList.toggle('open');
   });
 });
