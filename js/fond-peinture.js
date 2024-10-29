fe15f8d975c8948ee930ac83023837ec4e4bb330
Olivier Poulin <456crdv@gmail.com>
2024-10-29 15:17:55 -0400

    Coup de pinceau

--- js/fond-peinture.js
+++ js/fond-peinture.js
@@ -1,9 +1,7 @@
 var html = document.documentElement;
 var body = document.body;
 
-var neons = document.getElementsByClassName("fond-neon");
-//let solutionDuPauvre;
-//solutionDuPauvre = setInterval(modifierHauteur, 200);
+var peinture = document.getElementsByClassName("fond-peinture");
 
 //On appelle la fonction à chaque fois que la fenêtre bouge
 window.addEventListener("resize", modifierHauteur);
@@ -11,11 +9,11 @@ window.addEventListener("resize", modifierHauteur);
 //Appel initial de la fonction
 window.addEventListener("load", modifierHauteur);
 
-
 function modifierHauteur() {
     var height = Math.max(body.scrollHeight);
 
-    for (div of neons) {
+    for (div of peinture) {
         div.style.height = height + "px";
     }
+
 }
