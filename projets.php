ee00857a457e8b8859c1e9be15b8ec4cd352572d
EricNguyen0 <e1446191@cmaisonneuve.qc.ca>
2024-11-08 16:08:52 -0500

    page projet a faire _ plugin fds

--- projets.php
+++ projets.php
@@ -1,4 +1,17 @@
-<div class="container">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Projet</title>
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+    />
+    <link rel="stylesheet" href=" <?php echo get_template_directory_uri() . '/normalize.css'; ?> ">
+    <link rel="stylesheet" href=" <?php echo get_template_directory_uri() . '/style.css'; ?> ">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+  </head>
+
+<div class="container-projet">
       <div class="box">
         <span></span>
         <div class="content">
