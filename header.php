036d8d2dbbae6df2a7dad83c06863dc28bcb0a05
EricNguyen0 <e1446191@cmaisonneuve.qc.ca>
2024-11-09 00:41:25 -0500

    reinisialler le fameux button burger span avec 3 ligne click devient un x , maintenant faire en sorte que le menu apparait comme avant pour mobile

--- header.php
+++ header.php
@@ -18,22 +18,25 @@
   <div class="logo">
     <?php echo get_custom_logo(); ?>
   </div>
-  <!-- MENU -->
-  <label id="burger" for="chk_burger">
-    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#000">
-      <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
-    </svg>
-  </label>
+  <!-- BURGER FORMATTER AVEC ANIMATION -->
   <input id="chk_burger" type="checkbox">
+  <label id="burger" for="chk_burger">
+    <span></span>
+    <span></span>
+    <span></span>
+  </label>
+  <!-- Wordpress MENU pour que ca fonctionne avec nos styles -->
   <?php echo
   wp_nav_menu(array(
     "menu" => "principal",
     "container" => "nav",
     "container_class" => "menu-principal-container"
   ));
-  get_search_form()
+  // Le formulaire de recherche
+  get_search_form();
   ?>
 </header>
+<!-- Avoir access au JS -->
 <script src="<?php echo get_template_directory_uri() . '/js/menu.js'; ?>"></script>
 </body>
 </html>
