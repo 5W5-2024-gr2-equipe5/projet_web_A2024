eaf3b2c760467fa58cfea10bef6dbfdcf3d95380
EricNguyen0 <e1446191@cmaisonneuve.qc.ca>
2024-11-13 01:09:27 -0500

    pour que la video joue

--- front-page.php
+++ front-page.php
@@ -3,7 +3,7 @@
 <!-- Section Video -->
 <div class="video">
   <video autoplay loop muted>
-    <source src="<?php echo esc_url( 'http://tim2.local/wp-content/uploads/2024/11/WORK2.mp4' ); ?>" type="video/mp4" />
+    <source src="<?php echo esc_url( 'https://gftnth00.mywhc.ca/tim30/wp-content/uploads/2024/10/WORK2.mp4' ); ?>" type="video/mp4" />
     Your browser does not support the video tag.
   </video>
 </div>
