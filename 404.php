40e843c315d0fe994d92522b36feef71c99afd1b
David Rousseau <sylvainrousseau099@gmail.com>
2024-11-15 17:13:32 -0500

    404 creer sans style

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header() ?>
+
+<div class="container_404">
+    <div id="page_404">
+        <h1>Érreur 404</h1>
+        <p>la page que vous tentez d'accéder n'existe pas!</p>
+    </div>
+</div>
+
+<?php get_footer() ?>
