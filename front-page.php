6e1aea024ad28dc523cbf338cfd44e75627bea74
EricNguyen0 <e1446191@cmaisonneuve.qc.ca>
2024-11-26 16:24:30 -0500

    description du tim ajouter

--- front-page.php
+++ front-page.php
@@ -6,10 +6,16 @@
     <source src="<?php echo esc_url( 'https://gftnth00.mywhc.ca/tim30/wp-content/uploads/2024/10/WORK2.mp4' ); ?>" type="video/mp4" />
     Your browser does not support the video tag.
   </video>
-  <div class="video-overlay">
+</div>
+<div class="video-description">
     <h1>Technique d'intégration multimédia</h1>
-    <p>Collaboration, Création et Compétences Techniques
+    <h2>Collaboration, Création et Compétences Techniques</h2>
+    <p>
+        La technique d'intégration multimédia (TIM) du Collège de Maisonneuve forme des spécialistes en conception,
+        développement et intégration de contenus numériques interactifs.&nbsp;Les diplômés maîtrisent la création d’expériences utilisateur engageantes sur&nbsp; 
+        des plateformes variées, incluant sites web, jeux vidéo et autres plateformes numériques.
+        Le&nbsp;programme encourage une approche interdisciplinaire, où créativité et logique 
+        se rencontrent pour répondre aux besoins d’un marché en constante évolution.
     </p>
-  </div>
 </div>
 <?php get_footer(); ?>
