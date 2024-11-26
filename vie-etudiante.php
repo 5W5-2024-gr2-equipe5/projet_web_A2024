3c2f6b269aaaaa5f3a7d7d47cf2a1a850a1ffea5
Olivier Poulin <456crdv@gmail.com>
2024-11-26 16:24:38 -0500

    Le SASS de la page vie-etudiante est mostly fixed

--- vie-etudiante.php
+++ vie-etudiante.php
@@ -15,7 +15,7 @@ get_header(); // Include the WordPress header
 
     <div class="div-vie-etudiante" id="comite">
       <h2>Comité étudiant</h2>
-      <img src="<?php echo get_template_directory_uri(); ?>/images/VieEtudiante/cetim.png" alt="CÉTIM">
+      <img class="image-vie-etudiante" src="<?php echo get_template_directory_uri(); ?>/images/VieEtudiante/cetim.png" alt="CÉTIM">
       <p>
         Le comité étudiant du TIM est une organisation étudiante à but non lucratif 
         qui vise à rendre votre passage au TIM mémorable. Le comité est composé d'étudiants 
@@ -29,13 +29,15 @@ get_header(); // Include the WordPress header
       <h2>Évènements</h2>
       <div class="conteneur-event">
         <h3>Au collège</h3>
-        <p>
-          À Maisonneuve, nous sommes comme une grande famille et nous avons à cœur le bonheur de nos étudiants. 
-          Dans cette optique, le cégep offre plusieurs activités durant la session, incluant 
-          la fête de la rentrée, le dîner des programmes (qui vise à favoriser la socialisation entre étudiants), 
-          la fête d’halloween et plusieurs autres surprises que vous découvrirez le mardi et jeudi midi durant vos pauses.
-        </p>
-        <img src="<?php echo get_template_directory_uri(); ?>/images/Photo_arcade_vie_etudiante.jpg" alt="Arcade jeux en équipe">
+        <div>
+          <p>
+            À Maisonneuve, nous sommes comme une grande famille et nous avons à cœur le bonheur de nos étudiants. 
+            Dans cette optique, le cégep offre plusieurs activités durant la session, incluant 
+            la fête de la rentrée, le dîner des programmes (qui vise à favoriser la socialisation entre étudiants), 
+            la fête d’halloween et plusieurs autres surprises que vous découvrirez le mardi et jeudi midi durant vos pauses.
+          </p>
+          <img class="image-vie-etudiante" src="<?php echo get_template_directory_uri(); ?>/images/Photo_arcade_vie_etudiante.jpg" alt="Arcade jeux en équipe">
+        </div>
       </div>
 
       <div class="conteneur-event">
@@ -45,7 +47,7 @@ get_header(); // Include the WordPress header
           énormément d'activités tout au long de notre parcours. Le BienvenueTIM, les nombreuses Soirées gaming, 
           les dîners cinéma ou Jackbox, les jeux de société, quelques Game jams, des concours de design et bien d'autres.
         </p>
-        <img src="<?php echo get_template_directory_uri(); ?>/images/VieEtudiante/activite_Jackbox.jpg" alt="*Insérer caroussel ici*">
+        <img class="image-vie-etudiante" src="<?php echo get_template_directory_uri(); ?>/images/VieEtudiante/activite_Jackbox.jpg" alt="*Insérer caroussel ici*">
       </div>
     </div>
 
@@ -58,29 +60,22 @@ get_header(); // Include the WordPress header
         par session jusqu'à un maximum de 9000$, soit l’équivalent de vos 6 sessions obligatoires au cégep 
         pour compléter la technique. Pour y être admissible, rien de plus simple!
         Il vous suffit de:
-        
-        <ul>
+      </p>
+        <ul class="liste-requis">
           <li>Être résident au Québec et avoir la citoyenneté canadienne</li>
           <li>Réussir tous vos cours d’une session commencée et terminée à temps plein; 
               ou réussir tous vos cours d’une session commencée et terminée à temps partiel 
               tout en ayant le statut de personne réputée aux études à temps plein</li>
           <li>Ne pas dépasser le nombre de 6 sessions admissibles</li>
         </ul>
-        
+      <p>
         La bourse Perspective Québec peut être demandée après chaque session et est remise à toute personne 
         respectant les conditions et en faisant la réclamation par le site officiel du Québec. De plus, 
         notez que cette bourse pourra vous suivre à l'université si vous décidez de continuer vos études. 
         (Par exemple, grâce au programme Dec-Bac ou au passerelle!)
       </p>
 
-      <h3>Aide financière aux études (AFE)</h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed quis
-        repellendus voluptatem dolorum hic eligendi amet eum facilis commodi eos
-        explicabo, alias dolores obcaecati. Dolorem natus iusto beatae
-        repellendus asperiores!
-      </p>
-      <img src="<?php echo get_template_directory_uri(); ?>/images/VieEtudiante/etudiants_groupe_2.jpg" alt="Heureuse Surprise!">
+      <img class="image-vie-etudiante" src="<?php echo get_template_directory_uri(); ?>/images/VieEtudiante/etudiants_groupe_2.jpg" alt="Heureuse Surprise!">
     </div>
 
     <script src="<?php echo get_template_directory_uri(); ?>/js/marge-neon.js"></script>
