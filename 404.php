fe9a033dc49e9568cf64204339fde221d954f041
David Rousseau <sylvainrousseau099@gmail.com>
2024-11-26 16:32:31 -0500

    Page 404 style en cours

--- 404.php
+++ 404.php
@@ -4,6 +4,7 @@
     <div id="page_404">
         <h1>Erreur 404</h1>
         <p>la page que vous tentez d'accéder n'existe pas!</p>
+        <a href="/index.php">Revenir à l'acceuil <?php echo get_custom_logo(); ?></a>
     </div>
 </div>
 
