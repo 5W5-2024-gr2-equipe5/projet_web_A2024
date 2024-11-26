382b68e52ebaaef8e2212a055ce4d9e5c1ac005e
EricNguyen0 <e1446191@cmaisonneuve.qc.ca>
2024-11-26 15:18:36 -0500

    fixe couple de pages

--- front-page.php
+++ front-page.php
@@ -6,5 +6,10 @@
     <source src="<?php echo esc_url( 'https://gftnth00.mywhc.ca/tim30/wp-content/uploads/2024/10/WORK2.mp4' ); ?>" type="video/mp4" />
     Your browser does not support the video tag.
   </video>
+  <div class="video-overlay">
+    <h1>Technique d'intégration multimédia</h1>
+    <p>Collaboration, Création et Compétences Techniques
+    </p>
+  </div>
 </div>
 <?php get_footer(); ?>
